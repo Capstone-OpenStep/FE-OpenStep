27c0aa8a301ce0bbdea2179cd17e657aa7f2d2d6
allorak333 <allorak333@gmail.com>
2025-04-14 21:26:46 +0900

    feat : 사이드바 애니메이션
    헤더 우측 유저 프로필 사진 클릭 시 사이드 바 렌더링
    사이드 바 내부 버튼 중 테스트를 위해 임시적으로 로그아웃 시 signup페이지로 이동
    그 외 사이드 바에서 대시보드로 이동 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../assets/logo.png'
-
+import Sidebar from './Sidebar';
 
 const Header = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = () => setSidebarOpen(prev => !prev);
+
   const navigate = useNavigate();
   const onClickImg = () => {
     navigate(`/`);
@@ -14,7 +17,7 @@ const Header = () => {
     <div className={styles.cusRectangle2}>
       <div className={styles.cusFrame80}>
         <div className={styles.cusDiv2}>도움말</div>
-        <div className={styles.cusEllipse3}></div>
+        <div className={styles.cusEllipse3} onClick={toggleSidebar}></div>
       </div>
       <div className={styles.cusFrame79}>
         <img
@@ -26,6 +29,7 @@ const Header = () => {
         <div className={styles.cusDiv3}>커뮤니티</div>
         <div className={styles.cusDiv4}>랭킹</div>
       </div>
+      <Sidebar isOpen={isSidebarOpen} onClose={() => setSidebarOpen(false)} />
     </div>
   );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,22 +1,43 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Sidebar.module.css';
+import classNames from 'classnames';
 
-const Sidebar = () => {
+interface SidebarProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
+  const navigate = useNavigate();
+  const onClickDashboard = () => {
+    onClose();
+    navigate(`/dashboard`);
+  };
+  const onClickLogOut = () => {
+    onClose();
+    navigate(`/signup`);
+  };
+
+
   return (
-    <div className={styles.sidebar}>
+    <div className={classNames(styles.sidebar, {
+      [styles.open]: isOpen,
+      [styles.closed]: !isOpen,
+    })}>
       <div className={styles.avatar} />
       <div className={styles.username}>allorak333</div>
-      <div className={styles.closeButton}>x</div>
+      <div className={styles.closeButton} onClick={onClose}>x</div>
       <div className={styles.dividerHeader} />
 
-      <div className={styles.menuDashboard}>대시보드(My profile)</div>
+      <div className={styles.menuDashboard} onClick={onClickDashboard}>대시보드(My profile)</div>
       <div className={styles.menuProject}>내가 참여한 프로젝트(My Project)</div>
       <div className={styles.menuBookmarks}>북마크 프로젝트(My Bookmarks)</div>
       <div className={styles.menuSettings}>설정(Settings)</div>
       <div className={styles.menuGithub}>깃헙으로 이동(Github)</div>
 
       <div className={styles.dividerFooter} />
-      <div className={styles.menuLogout}>로그아웃(Sign out)</div>
+      <div className={styles.menuLogout} onClick={onClickLogOut}>로그아웃(Sign out)</div>
     </div>
   );
 };
