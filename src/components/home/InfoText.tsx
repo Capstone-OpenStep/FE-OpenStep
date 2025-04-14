c5a1986f3408808da2dad6515bf2ee10b233170d
allorak333 <allorak333@gmail.com>
2025-04-14 21:07:12 +0900

    feat : 홈페이지 로직 구현
    검색 문구를 저장할 state(query) 를 새로 생성
    그 외 mode 와 query를 함수 컴포넌트에 전달하여 각 mode 당 다른 문구 렌더링 하도록 설정

--- src/components/home/InfoText.tsx
+++ src/components/home/InfoText.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './InfoText.module.css';
 import SwitchIcon from '../../assets/switch.svg';
 
 
 interface InfoTextProps {
   mode: number;
+  query: string;
   setMode: React.Dispatch<React.SetStateAction<number>>;
 }
 
 
-const InfoText: React.FC<InfoTextProps> = ({mode, setMode}) => {
+const InfoText: React.FC<InfoTextProps> = ({ mode, query, setMode }) => {
   const switchMode = () => {
     if (mode === 0)
       setMode(1);
@@ -19,10 +20,11 @@ const InfoText: React.FC<InfoTextProps> = ({mode, setMode}) => {
       setMode(0);
   }
 
+
   return (
     <div className={styles.infoTextContainer}>
       <div className={styles.infoTextRow}>
-        <div className={styles.infoTextContent}>
+        {mode === 0 ? (<><div className={styles.infoTextContent}>
           <span className={styles.name}>송민혁</span>
           <span className={styles.greeting}>
             님을 위해
@@ -33,12 +35,35 @@ const InfoText: React.FC<InfoTextProps> = ({mode, setMode}) => {
           <span className={styles.webText}>Web</span>
           <span className={styles.serviceText}>서비스를 찾아봤어요.</span>
         </div>
-        <div onClick={switchMode} style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}}>
-          <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
-          <span className={styles.switchIconText}>트랜딩 보기</span>
-        </div>
+          <div onClick={switchMode} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', marginLeft:'auto'  }}>
+            <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
+            <span className={styles.switchIconText}>트랜딩 보기</span>
+          </div></>) : mode === 1 ? (
+            <><div className={styles.infoTextContent}>
+              <span className={styles.trendingText}>
+                다른 개발자들은 이런 것에 관심이 많아요
+              </span>
+            </div>
+              <div onClick={switchMode} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', marginLeft:'auto' }}>
+                <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
+                <span className={styles.switchIconText}>추천 보기</span>
+              </div></>) : (<><div className={styles.infoTextContent}>
+              <span className={styles.queryText}>
+                "{query}"
+              </span>
+              <span className={styles.trendingText}>
+                {" 에 대한 검색 결과예요"}
+              </span>
+            </div>
+              <div onClick={switchMode} style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', marginLeft:'auto'  }}>
+                <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
+                <span className={styles.switchIconText}>추천/트랜딩 보기</span>
+              </div></>
+
+            )}
+
       </div>
-      {mode <= 1 ? ( <span className={styles.subText}>첫 기여, 아래 프로젝트로 시작해보세요!</span>) : (<></>)}
+      {mode <= 1 ? (<span className={styles.subText}>첫 기여, 아래 프로젝트로 시작해보세요!</span>) : (<></>)}
     </div>
   );
 };
