7905685b8c3c1376eaf77eb650f3b80ce53e31d9
allorak333 <allorak333@gmail.com>
2025-04-14 17:40:02 +0900

    feat : 홈 버튼 기능
    상단 헤더 좌측 이미지 클릭 시 홈 경로로 이동하도록 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../assets/logo.png'
 
+
 const Header = () => {
+  const navigate = useNavigate();
+  const onClickImg = () => {
+    navigate(`/`);
+  };
+
   return (
     <div className={styles.cusRectangle2}>
       <div className={styles.cusFrame80}>
@@ -13,6 +20,8 @@ const Header = () => {
         <img
           className={styles.cusImage1}
           src={logo}
+          onClick={onClickImg}
+          style={{cursor:'pointer'}}
         />
         <div className={styles.cusDiv3}>커뮤니티</div>
         <div className={styles.cusDiv4}>랭킹</div>
