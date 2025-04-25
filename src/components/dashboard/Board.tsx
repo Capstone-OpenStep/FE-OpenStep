e4ee21d9fef873d6efecfeeaedb380aff5e1e769
allorak333 <allorak333@gmail.com>
2025-04-25 18:25:24 +0900

    refactor : 보드 컴포넌트 리팩토링
    보드 컴포넌트에 앞으로 overview와 bookmark가 추가될 예정이기 때문에
    상단 버튼으로 전환되는 요소를 컴포넌트로 분리

--- src/components/dashboard/Board.tsx
+++ src/components/dashboard/Board.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import styles from './Board.module.css';
-import arrow from '../../assets/arrowDown.svg'
-import setting from '../../assets/setting.svg'
-import Repository from './Repository'
 import overview from '../../assets/overview.svg'
 import list from '../../assets/list.svg'
 import bookmark from '../../assets/bookmark.svg'
+import TaskList from './TaskList'
+
 
 const Board: React.FC = () => {
 
@@ -31,34 +30,7 @@ const Board: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className={styles.boardBody}>
-        <div style={{ top: 17, width: 795, height: 35, position: 'relative', borderRadius: 5 }}>
-          <div style={{ width: 136, height: 34, left: 0, top: 1, position: 'absolute', display: 'flex', alignItems: 'center', background: '#DFDDFF', overflow: 'hidden', borderRadius: 5 }}>
-            <div style={{ left: 15, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Noto Sans KR', fontWeight: '500', wordWrap: 'break-word' }}>시간순</div>
-            <div data-size="32" style={{ width: 32, height: 32, left: 104, position: 'absolute', overflow: 'hidden' }}>
-              <img src={arrow} />
-            </div>
-          </div>
-          <div style={{ width: 136, height: 34, left: 162, top: 1, position: 'absolute', display: 'flex', alignItems: 'center', background: '#DFDDFF', overflow: 'hidden', borderRadius: 5 }}>
-            <div style={{ left: 15, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Noto Sans KR', fontWeight: '500', wordWrap: 'break-word' }}>상태</div>
-            <div data-size="32" style={{ width: 32, height: 32, left: 104, position: 'absolute', overflow: 'hidden' }}>
-              <img src={arrow} />
-            </div>
-          </div>
-          <div style={{ width: 179, height: 34, left: 616, top: 0, position: 'absolute', display: 'flex', alignItems: 'center', background: 'rgba(125.03, 123.09, 156.06, 0.39)', overflow: 'hidden', borderRadius: 5 }}>
-            <div style={{ left: 15, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Noto Sans KR', fontWeight: '500', wordWrap: 'break-word' }}>필터 및 정렬 초기화</div>
-            <div style={{ width: 24, height: 24, left: 150, position: 'absolute' }}>
-              <img src={setting} />
-            </div>
-          </div>
-        </div>
-        <div className={styles.repositoryList}>
-          <Repository />
-          <Repository />
-          <Repository />
-          <Repository />
-        </div>
-      </div>
+        <TaskList></TaskList>
     </div>
   );
 };
