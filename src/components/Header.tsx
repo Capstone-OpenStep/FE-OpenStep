2425beee1522fa2858bd5966bd3b64671c80ee5c
allorak333 <allorak333@gmail.com>
2025-05-03 22:42:16 +0900

    feat : 헤더 우측 상단 조건부 렌더링
    로그인 페이지에서 사용자 프로필은 없기 때문에 get started 를 추가하고
    useLocation을 통해  로그인 페이지에서만 렌더링 되도록 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import styles from './Header.module.css';
 import logo from '../assets/logo.png';
 import Sidebar from './Sidebar';
@@ -9,14 +9,23 @@ const Header = () => {
   const toggleSidebar = () => setSidebarOpen(prev => !prev);
 
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === '/login';
   const onClickImg = () => navigate(`/`);
   const onClickRanking = () => navigate(`/ranking`);
 
   return (
     <div className={styles.headerContainer}>
       <div className={styles.headerRightSection}>
-        <div className={styles.helpText}>도움말</div>
-        <div className={styles.profileButton} onClick={toggleSidebar}></div>
+      {isLoginPage ? (
+          <button className={styles.getStartedButton}>Get Started</button>
+        ) : (
+          <>
+            <div className={styles.helpText}>도움말</div>
+            <div className={styles.profileButton} onClick={toggleSidebar}></div>
+          </>
+        )}
       </div>
       <div className={styles.headerLeftSection}>
         <img
