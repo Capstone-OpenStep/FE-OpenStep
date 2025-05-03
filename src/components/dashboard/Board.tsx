f85f435a0a97e008b76763904d5388caea04bf80
allorak333 <allorak333@gmail.com>
2025-05-03 19:40:21 +0900

    feat : 대시보드  상단 버튼 구현
    대시 보드 상단 선택 버튼 추가하여  각 버튼 클릭 시 해당하는 컴포넌트 렌더링 로직 추가
    상단 버튼 호버 시 색 변화 및 클릭 시 색 변화로 UI 개선

--- src/components/dashboard/Board.tsx
+++ src/components/dashboard/Board.tsx
@@ -4,33 +4,48 @@ import overview from '../../assets/overview.svg'
 import list from '../../assets/list.svg'
 import bookmark from '../../assets/bookmark.svg'
 import TaskList from './TaskList'
-
+import BookmarkList from './BookmarkList';
+import Overview from './Overview'
 
 const Board: React.FC = () => {
+  const [select, setSelect] = useState<number>(0);
 
   return (
     <div>
       <div className={styles.buttonList}>
-        <div className={styles.selectButton}>
+        <div
+          className={`${styles.selectButton} ${select === 0 ? styles.selected : ''}`}
+          onClick={() => setSelect(0)}
+        >
           <div className={styles.buttonDescription}>
-            <img src={overview} style={{ height: 20, width: 20 }}></img>
+            <img src={overview} style={{ height: 20, width: 20 }} />
             <span className={styles.buttonText}>overview</span>
           </div>
         </div>
-        <div className={styles.selectButton}>
+
+        <div
+          className={`${styles.selectButton} ${select === 1 ? styles.selected : ''}`}
+          onClick={() => setSelect(1)}
+        >
           <div className={styles.buttonDescription}>
-            <img src={list} style={{ height: 20, width: 20 }}></img>
+            <img src={list} style={{ height: 20, width: 20 }} />
             <span className={styles.buttonText}>projects</span>
           </div>
         </div>
-        <div className={styles.selectButton}>
+
+        <div
+          className={`${styles.selectButton} ${select === 2 ? styles.selected : ''}`}
+          onClick={() => setSelect(2)}
+        >
           <div className={styles.buttonDescription}>
-            <img src={bookmark} style={{ height: 20, width: 20 }}></img>
+            <img src={bookmark} style={{ height: 20, width: 20 }} />
             <span className={styles.buttonText}>bookmark</span>
           </div>
         </div>
       </div>
-        <TaskList></TaskList>
+      {select === 0 && <Overview />}
+      {select === 1 && <TaskList />}
+      {select === 2 && <BookmarkList />}
     </div>
   );
 };
