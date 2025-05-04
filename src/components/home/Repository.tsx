7e44b831f3df0ee0d4b47f9adbaf5a6712f8b248
allorak333 <allorak333@gmail.com>
2025-05-04 16:22:56 +0900

    feat : 북마크 추가 버튼
    메인 화면 (홈페이지)의 레포지토리에 북마크 버튼 추가
    assets 파일이 증가하여 파일 수를 줄이기 위해 인라인 코드로 svg 색상을 변경하는 로직으로 구현

--- src/components/home/Repository.tsx
+++ src/components/home/Repository.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styles from './Repository.module.css';
 import star from '../../assets/star.svg'
+
+
+const SvgToggleFill: React.FC = () => {
+  const [isFilled, setIsFilled] = useState<boolean>(false);
+
+  return (
+    <div
+      onClick={(e) => {
+        e.stopPropagation(); // 클릭 이벤트 전파 방지
+        setIsFilled((prev) => !prev);
+      }}
+      style={{ cursor: 'pointer', width: '35px', height: '35px', marginLeft:'auto' }}
+    >
+      <svg
+        width="35"
+        height="35"
+        viewBox="0 0 35 35"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M27.7083 30.625L17.5 23.3333L7.29163 30.625V7.29167C7.29163 6.51812 7.59892 5.77625 8.1459 5.22927C8.69288 4.68229 9.43474 4.375 10.2083 4.375H24.7916C25.5652 4.375 26.307 4.68229 26.854 5.22927C27.401 5.77625 27.7083 6.51812 27.7083 7.29167V30.625Z"
+          fill={isFilled ? '#FFDC4E' : '#FFFFFF'}
+          stroke="#1E1E1E"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          style={{ transition: 'fill 0.5s ease' }}
+        />
+      </svg>
+    </div>
+  );
+};
+
+
 
 const Repostory = () => {
   const navigate = useNavigate();
@@ -14,7 +49,10 @@ const Repostory = () => {
       <div className={styles.container} onClick={onClickContainer}>
         <div className={styles.bg}></div>
         <div className={styles.header}>
-          <div className={styles.title}>jestjs/jest</div>
+          <div className={styles.headerTop}>
+            <div className={styles.title}>jestjs/jest</div>
+            <SvgToggleFill></SvgToggleFill>
+          </div>
           <div className={styles.description}>
             표준 웹 기술(HTML, CSS, JS, WASM)을 최대로 활용하는 새로운 웹 프레임워크입니다.<br /><br />
             복잡한 도구 없이 작고 빠른 웹앱을 만들 수 있어요.
