025ad555dc32934464ff4d1993e6e5f415917c80
allorak333 <allorak333@gmail.com>
2025-05-09 23:50:23 +0900

    fix : react router 변경
    최신 버전에서 사용되지 않을 예정인 BrowserRouter에서 createBroserRouter로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./styles.css";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import { AuthProvider } from './components/AuthContext';
 import Header from "./components/Header";
 import Home from "./pages/Home"
 import SignUp from "./pages/SignUp";
@@ -10,21 +12,42 @@ import Dashboard from "./pages/Dashboard"
 import Ranking from "./pages/Ranking"
 import Login from "./pages/Login"
 
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "signup", element: <SignUp /> },
+        { path: "project", element: <Project /> },
+        { path: "issue", element: <Issue /> },
+        { path: "dashboard", element: <Dashboard /> },
+        { path: "ranking", element: <Ranking /> },
+        { path: "login", element: <Login /> },
+      ],
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+    },
+  }
+);
+
 export default function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/signup" element={<SignUp />}></Route>
-          <Route path="/project" element={<Project />}></Route>
-          <Route path="/issue" element={<Issue />}></Route>
-          <Route path="/dashboard" element={<Dashboard />}></Route>
-          <Route path="/ranking" element={<Ranking />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   );
 }
