05c14278e214bb5c60c22c0f94782abb488452e5
allorak333 <allorak333@gmail.com>
2025-05-20 22:38:41 +0900

    fix : 추천 이슈 dto 수정
    추천 이슈와 트랜딩 이슈의 dto가 달라 interface 추가 후 수정

--- src/api/issue.ts
+++ src/api/issue.ts
@@ -8,17 +8,26 @@ interface TrendingIssueResponse {
   result: Issue[];
 }
 
+interface RecommendedIssueResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: {
+    issueList: Issue[];
+  };
+}
+
 export const getTrendingIssues = async (): Promise<Issue[]> => {
   const response = await api.get<TrendingIssueResponse>("/issues/trending");
   return response.data.result;
 };
 
 export const getRecommendedIssues = async (): Promise<Issue[]> => {
-  const response = await api.get<TrendingIssueResponse>("/issues/suggest");
+  const response = await api.get<RecommendedIssueResponse>("/issues/suggest");
   if (!response.data.isSuccess) {
     const err = new Error(response.data.message);
     (err as any).code = response.data.code;
     throw err;
   }
-  return response.data.result;
+  return response.data.result.issueList;
 };
