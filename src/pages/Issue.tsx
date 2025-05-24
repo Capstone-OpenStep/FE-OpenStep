ed257b785c04e135b5606fd755cbc9d7e208551b
allorak333 <allorak333@gmail.com>
2025-05-24 19:12:26 +0900

    feat : 이슈 페이지 주소 쿼리 기능
    태스크 진행 중 새로고침 해도 해당 정보를 다시 불러올 수 있도록 issue id를 쿼리로 접근

--- src/pages/Issue.tsx
+++ src/pages/Issue.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth } from '../components/AuthContext'
 import styles from './Issue.module.css';
 import Summary from '../components/project/Summary';
 import logo from '../assets/projectLogo.svg'
@@ -8,7 +10,25 @@ import Guide from '../components/issue/Guide'
 
 const Project: React.FC = () => {
     const [stage, setStage] = useState<number>(1);
-    
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { isLoggedIn, setIsLoggedIn } = useAuth();
+
+    useEffect(() => {
+        if (isLoggedIn == false) {
+            console.log("???");
+            navigate('/');
+        }
+        const query = location.search;
+        const searchParams = new URLSearchParams(query);
+
+        const issueId = searchParams.get('issueId');
+        if (issueId === null) {
+            navigate('/');
+        }
+
+    }, [ location ])
+
     return (
         <div className={styles.body}>
             <div className={`${styles.section} ${styles.sectionLeft}`}>
