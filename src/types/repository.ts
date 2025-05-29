537d61496e1b04cefaf056c1abc9a9b1892abb17
allorak333 <allorak333@gmail.com>
2025-05-29 11:07:02 +0900

    fix : api 타입 경로 수정
    api에서 사용하는 타입을 하나의 파일로 병합

--- src/types/repository.ts
+++ src/types/repository.ts
@@ -12,3 +12,20 @@ export interface Repository {
   githubUrl: string;
   description: string;
 }
+
+export interface RepositoryDescription {
+  repoId: number;
+  repoName: string;
+  summary: string;
+  ownerName: string;
+  description: string;
+  language: string;
+  stars: number;
+  watchers: number;
+  forks: number;
+  openIssues: number;
+  closedIssues: number;
+  beginnerIssueCount: number;
+  githubUrl: string;
+  readmeUrl: string;
+}
