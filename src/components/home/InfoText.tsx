4019ec4b95a90394d7fbbfb0c2c26c677f97d7fd
allorak333 <allorak333@gmail.com>
2025-06-01 23:23:53 +0900

    feat : user 이름 관심 도메인 렌더링
    메인 화면에서 user이름과 관심 도메인 렌더링

--- src/components/home/InfoText.tsx
+++ src/components/home/InfoText.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { useAuth } from "../AuthContext"; 
+import React, { useEffect, useState } from 'react';
+import { useAuth } from "../AuthContext";
 import styles from './InfoText.module.css';
 import SwitchIcon from '../../assets/switch.svg';
 import ErrorModal from '../ErrorModal';
@@ -8,15 +8,17 @@ interface InfoTextProps {
   mode: number;
   query: string;
   setMode: React.Dispatch<React.SetStateAction<number>>;
+  domains: string[];
 }
 
 
-const InfoText: React.FC<InfoTextProps> = ({ mode, query, setMode }) => {
+const InfoText: React.FC<InfoTextProps> = ({ mode, query, setMode, domains }) => {
   // 로그인 여부
   const { isLoggedIn } = useAuth();
   // 에러 모달
   const [showModal, setShowModal] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
+  const [username, setUsername] = useState<string>('');
 
   const switchMode = () => {
     if (mode === 0) {
@@ -36,19 +38,36 @@ const InfoText: React.FC<InfoTextProps> = ({ mode, query, setMode }) => {
     }
   }
 
+  useEffect(() => {
+    const username = sessionStorage.getItem('username');
+    if (username != null) {
+      setUsername(username);
+    }
+  }, [isLoggedIn])
+
+  const renderDomains = () => {
+    return domains.map((domain, index) => {
+      const isLast = index === domains.length - 1;
+      return (
+        <React.Fragment key={domain}>
+          <span className={styles.reactText}>{domain}</span>
+          {!isLast && <span className={styles.comma}>, </span>}
+        </React.Fragment>
+      );
+    });
+  };
 
   return (
     <div className={styles.infoTextContainer}>
       <div className={styles.infoTextRow}>
         {mode === 0 ? (<><div className={styles.infoTextContent}>
-          <span className={styles.name}>송민혁</span>
+          <span className={styles.name}>{username}</span>
           <span className={styles.greeting}>
             님을 위해
             <br />
           </span>
-          <span className={styles.reactText}>React</span>
-          <span className={styles.comma}> , </span>
-          <span className={styles.webText}>Web</span>
+          {renderDomains()}
+          <br />
           <span className={styles.serviceText}>서비스를 찾아봤어요.</span>
         </div>
           <div onClick={switchMode} className={styles.switchModeButton}>
@@ -64,28 +83,28 @@ const InfoText: React.FC<InfoTextProps> = ({ mode, query, setMode }) => {
                 <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
                 <span className={styles.switchIconText}>추천 보기</span>
               </div></>) : (<><div className={styles.infoTextContent}>
-              <span className={styles.queryText}>
-                "{query}"
-              </span>
-              <span className={styles.trendingText}>
-                {" 에 대한 검색 결과예요"}
-              </span>
-            </div>
-              <div onClick={switchMode} className={styles.switchModeButton} >
-                <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
-                <span className={styles.switchIconText}>추천/트랜딩 보기</span>
-              </div></>
+                <span className={styles.queryText}>
+                  "{query}"
+                </span>
+                <span className={styles.trendingText}>
+                  {" 에 대한 검색 결과예요"}
+                </span>
+              </div>
+                <div onClick={switchMode} className={styles.switchModeButton} >
+                  <img src={SwitchIcon} className={styles.switchIconImg} alt="Switch Icon" />
+                  <span className={styles.switchIconText}>추천/트랜딩 보기</span>
+                </div></>
 
-            )}
+        )}
 
       </div>
       {mode <= 1 ? (<span className={styles.subText}>첫 기여, 아래 프로젝트로 시작해보세요!</span>) : (<></>)}
       <ErrorModal
-          show={showModal}
-          title="오류"
-          message={modalMessage}
-          confirmText="확인"
-          onConfirm={() => setShowModal(false)}
+        show={showModal}
+        title="오류"
+        message={modalMessage}
+        confirmText="확인"
+        onConfirm={() => setShowModal(false)}
       />
     </div>
   );
