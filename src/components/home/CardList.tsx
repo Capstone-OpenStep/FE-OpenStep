d0af5a5030e8dc16f184b1b77f8ef230e5f62db2
allorak333 <allorak333@gmail.com>
2025-06-01 22:19:54 +0900

    feat : 로딩 기능 구현
    트랜딩/추천 이슈 api의 응답 속도가 늦기 때문에 그 중간에 스켈레톤 로딩 추가

--- src/components/home/CardList.tsx
+++ src/components/home/CardList.tsx
@@ -6,21 +6,43 @@ import IssueItem from './IssueItem'
 import { Repository as RepoType } from '../../types/repository';
 import { Issue as IssueType } from '../../types/issue';
 
+
+const SkeletonIssueItem: React.FC = () => {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.titleSkeleton}></div>
+      <div className={styles.summarySkeleton}></div>
+      <div className={styles.tagsSkeleton}>
+        <div className={styles.tag}></div>
+        <div className={styles.tag}></div>
+        <div className={styles.tag}></div>
+      </div>
+      <div className={styles.footerSkeleton}></div>
+    </div>
+  );
+};
+
 interface CardListProps {
-  active: boolean;
-  setActive: React.Dispatch<React.SetStateAction<boolean>>;
   issues: IssueType[];
+  isLoading: boolean;
 }
 
-const CardList: React.FC<CardListProps> = ({ active, setActive, issues }) => {
+const CardList: React.FC<CardListProps> = ({ issues, isLoading }) => {
   return (
     <div className={styles.cardListContainer}>
-      {/* <ToggleSwitch active={active} setActive={setActive} /> */}
+      {isLoading ? (
+        <>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <SkeletonIssueItem key={index} />
+          ))}
+        </>
+      ) : (
         <>
           {issues.map((issue) => (
             <IssueItem key={issue.issueId} issue={issue} />
           ))}
         </>
+      )}
     </div>
   );
 };
