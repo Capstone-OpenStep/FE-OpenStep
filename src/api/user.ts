b966adaa79e4330ec06d1e57ed0717f2c7b1c2fa
allorak333 <allorak333@gmail.com>
2025-06-04 22:00:29 +0900

    feat : 랭킹 api 추가

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,6 @@
 import api from "./client";
 import {  } from "../types/issue";
+import { RankUser } from "../types/rank"
 
 export interface getDomainResponse {
   isSuccess: boolean;
@@ -14,3 +15,17 @@ export const getDomains = async (): Promise<string[]> => {
   const response = await api.get<getDomainResponse>(`/member/domains`);
   return response.data.result.domains;
 };
+
+export interface getRanksResponse {
+  isSuccess: boolean;
+  code: string;
+  message: string;
+  result: {
+    ranks: RankUser[];
+  };
+}
+
+export const getRanks = async (): Promise<RankUser[]> => {
+  const response = await api.get<getRanksResponse>(`/rank/all`);
+  return response.data.result.ranks;
+}
