73e845be677cbe2020208063a5fb130948463ad6
allorak333 <allorak333@gmail.com>
2025-06-08 19:38:59 +0900

    feat : 업적 api 추가

--- src/types/user.ts
+++ src/types/user.ts
@@ -13,3 +13,15 @@ export interface UserProfile {
   followersCount: number;
   followingCount: number;
 }
+
+export interface Acheievement {
+  id: number;
+  type: "FIRST_COMMIT" | "BUF_HUNTER" | "PR_MASTER" | "CONSISTENT_DEV" | "EXPLORER_LV1" | "EXPLORER_LV2" | "EXPLORER_LV3" | "MENTOR";
+  title: string;
+  description: string;
+  unlocked: boolean;
+  unlockedAt: string;
+  currentProgress: number;
+  targetCount: number;
+  newlyUnlocked: boolean;
+}
